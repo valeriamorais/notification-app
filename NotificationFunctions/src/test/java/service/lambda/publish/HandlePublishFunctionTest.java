33bf341253aff7539b469cdf5e28712e5a01156e
Valeria Morais <valeriamorais.tech@gmail.com>
2022-10-26 12:52:59 -0300

    criacao das lambdas

---
+++ NotificationFunctions/src/test/java/service/lambda/publish/HandlePublishFunctionTest.java
@@ -0,0 +1,10 @@
+package service.lambda.publish;
+
+import org.junit.Test;
+
+public class HandlePublishFunctionTest {
+  @Test
+  public void successfulResponse() {
+    //TODO - implement HandlePublishFunctionTest
+  }
+}
---
+++ NotificationFunctions/src/test/java/service/lambda/publish/HandleScheduleFunctionTest.java
@@ -0,0 +1,10 @@
+package service.lambda.publish;
+
+import org.junit.Test;
+
+public class HandleScheduleFunctionTest {
+  @Test
+  public void successfulResponse() {
+    //TODO - implement HandleScheduleFunctionTest
+  }
+}
