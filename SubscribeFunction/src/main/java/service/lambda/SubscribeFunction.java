590497b5abdee5def44339b36cd0fa6a78bc007c
Valeria Morais <valeriamorais.tech@gmail.com>
2022-10-26 08:10:19 -0300

    new lambda subscribefunction

---
+++ SubscribeFunction/src/main/java/service/lambda/SubscribeFunction.java
@@ -0,0 +1,38 @@
+package service.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class SubscribeFunction implements RequestStreamHandler {
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException{
+
+        //API para manipulacao de JSONs
+        JsonObject responseJson = new JsonObject();
+        JsonObject responseBody = new JsonObject();
+        JsonObject headerJson = new JsonObject();
+
+        //propriedade mensagem na resposta
+        responseBody.addProperty("message", "subscription done successfully");
+
+        // um header aleatório na resposta para API Gateway
+        headerJson.addProperty("x-custom-header", "");
+
+        // Http status code 200 OK
+        responseJson.addProperty("statusCode", 200);
+        responseJson.add("headers", headerJson);
+        responseJson.addProperty("body", responseBody.toString());
+
+        // serializa o JSON para um OutputStream
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write(responseJson.toString());
+        writer.close();
+    }
+}
